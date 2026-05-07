32e36076b29d9a34f7c20643c6fe9cc04fc0a7c9
amkma <amkma978@gmail.com>
2026-05-08 01:33:37 +0300

    Initial Commit

---
+++ backend/src/main/java/com/assettrack/sw_final_project_assettrack/SwFinalProjectAssettrackApplication.java
@@ -0,0 +1,13 @@
+package com.assettrack.sw_final_project_assettrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwFinalProjectAssettrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwFinalProjectAssettrackApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/assettrack/sw_final_project_assettrack/SwFinalProjectAssettrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assettrack.sw_final_project_assettrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwFinalProjectAssettrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
